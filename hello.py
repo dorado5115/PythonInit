afa3690d87b7859d3285bdd4336d3c07724e751b
Sofia <doradodx@gmail.com>
2020-10-19 17:08:34 -0500

    Se comento el codigo

--- hello.py
+++ hello.py
@@ -2,14 +2,19 @@ print("Hola Mundo\n" * 5)#se imprime 5 veces hola mundo
 
 x = 8
 
+#se imprime x
 print(x)
 
 y = "Holaa y Adios" #branch Test
 
+#se imprime y
 print(y)
 
 m = 5.50 + 6.62
 
+#se imprime m
 print(m)
 
+#Se imprime los 3 datos
 print(f"Este es x = {x}, este es y = {y} este es m = {m}")
+
